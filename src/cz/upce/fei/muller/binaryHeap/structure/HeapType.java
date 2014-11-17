5ae78f80e32e5239e4ad191cb6e66038e7a1819b
Vojtěch Müller <muller.voj@gmail.com>
2014-11-17 21:06:19 +0100

    changeHeap type to class for better values.

--- src/cz/upce/fei/muller/binaryHeap/structure/HeapType.java
+++ src/cz/upce/fei/muller/binaryHeap/structure/HeapType.java
@@ -3,6 +3,15 @@ package cz.upce.fei.muller.binaryHeap.structure;
 /**
  * @author Vojtěch Müller
  */
-enum HeapType {
-    MIN,MAX
+public final class HeapType {
+
+    public final Integer compareValue;
+
+    public static final HeapType MIN = new HeapType(-1);
+
+    public static final HeapType MAX = new HeapType(1);
+
+    private HeapType(Integer compareValue) {
+        this.compareValue = compareValue;
+    }
 }
