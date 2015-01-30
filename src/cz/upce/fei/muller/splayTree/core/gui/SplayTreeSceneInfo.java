2bd63015b92a048deac0fb97c7c18c950eb49bec
Vojtěch Müller <muller.voj@gmail.com>
2015-01-30 09:35:58 +0100

    Creating default Treap values.

---
+++ src/cz/upce/fei/muller/splayTree/core/gui/SplayTreeSceneInfo.java
@@ -0,0 +1,29 @@
+package cz.upce.fei.muller.splayTree.core.gui;
+
+import cz.commons.graphics.IGraphics;
+import cz.upce.fei.common.gui.utils.SceneInfo;
+
+/**
+ * @author Vojtěch Müller
+ */
+public class SplayTreeSceneInfo implements SceneInfo {
+    @Override
+    public double getWidth() {
+        return 600* IGraphics.PLATFORM_SCALE;
+    }
+
+    @Override
+    public double getHeight() {
+        return 930 * IGraphics.PLATFORM_SCALE;
+    }
+
+    @Override
+    public String getTitle() {
+        return "Splay";
+    }
+
+    @Override
+    public Integer getMinSceneWith() {
+        return 200;
+    }
+}
