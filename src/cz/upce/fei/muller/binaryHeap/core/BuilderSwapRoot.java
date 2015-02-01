fbe6b0255a01188b6719cfc66d8987a31945ad8f
Vojtěch Müller <muller.voj@gmail.com>
2015-02-01 08:57:22 +0100

    Finished build swap root with leaf..:0

--- src/cz/upce/fei/muller/binaryHeap/core/BuilderSwapRoot.java
+++ src/cz/upce/fei/muller/binaryHeap/core/BuilderSwapRoot.java
@@ -3,10 +3,13 @@ package cz.upce.fei.muller.binaryHeap.core;
 import cz.commons.animation.StepEventHandler;
 import cz.commons.graphics.LineElement;
 import cz.commons.utils.FadesTransitionBuilder;
+import cz.commons.utils.transitions.RelativeTranslateTransition;
 import cz.upce.fei.common.graphics.NodePosition;
 import cz.upce.fei.muller.binaryHeap.graphics.BinaryHeapNode;
 import javafx.animation.FadeTransition;
 import javafx.animation.ParallelTransition;
+import javafx.animation.SequentialTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
@@ -29,20 +32,31 @@ public class BuilderSwapRoot extends BuilderSwapNode {
 
     @Override
     public ParallelTransition getAnimation() {
-        ParallelTransition pt = super.getAnimation();
-        pt.getChildren().add(0, getFadeTransition());
+        ParallelTransition pt = getMovingElements();
+        SequentialTransition sq = new SequentialTransition(getFades(true),pt,getFades(false));
         pt.setOnFinished(setFinishedHandler());
-        return new ParallelTransition(pt);
+        return new ParallelTransition(sq);
     }
 
     private EventHandler<ActionEvent> setFinishedHandler() {
         return new SwapRootEndEventHandler(lineParentOfLeaf,leafParent,nodeFirst,nodeSecond);
     }
 
-    private FadeTransition getFadeTransition() {
+    private FadeTransition getFadeTransition(int from,int to) {
         BinaryHeapNode left= leafParent.getChild(NodePosition.LEFT);
         lineParentOfLeaf = leafParent.getChildLine(left!=null && left.getElementId()==nodeSecond.getElementId()?NodePosition.LEFT:NodePosition.RIGHT);
-        return FadesTransitionBuilder.getTransition(lineParentOfLeaf,Duration.millis(1),1,0);
+        return FadesTransitionBuilder.getTransition(lineParentOfLeaf,Duration.millis(1),from,to);
+    }
+    private FadeTransition getFadeTransitionRoot(NodePosition position,int from,int to) {
+        return FadesTransitionBuilder.getTransition(nodeFirst.getChildLine(position),Duration.millis(1),from,to);
     }
 
+    private ParallelTransition getFades(boolean forward){
+        int from = forward?1:0;
+        int to = forward?0:1;
+        FadeTransition ft =getFadeTransition(from,to);
+        FadeTransition ft1 = getFadeTransitionRoot(NodePosition.LEFT,from,to);
+        FadeTransition ft2 = getFadeTransitionRoot(NodePosition.RIGHT,from,to);
+        return new ParallelTransition(ft,ft1,ft2);
+    }
 }
--- src/cz/upce/fei/muller/binaryHeap/core/SwapRootEndEventHandler.java
+++ src/cz/upce/fei/muller/binaryHeap/core/SwapRootEndEventHandler.java
@@ -5,6 +5,7 @@ import cz.commons.graphics.LineElement;
 import cz.commons.utils.FadesTransitionBuilder;
 import cz.upce.fei.common.graphics.NodePosition;
 import cz.upce.fei.muller.binaryHeap.graphics.BinaryHeapNode;
+import cz.upce.fei.muller.binaryHeap.structure.BinaryHeap;
 import javafx.event.ActionEvent;
 import javafx.util.Duration;
 
@@ -14,33 +15,51 @@ import javafx.util.Duration;
 public class SwapRootEndEventHandler extends StepEventHandler {
 
     final BinaryHeapNode parentLeaf;
-    final LineElement lineElement;
+    final LineElement lineElementToLeaf;
 
-    final BinaryHeapNode first;
-    final BinaryHeapNode second;
-
-    final boolean isLeft = false;
+    final BinaryHeapNode oldRoot;
+    final BinaryHeapNode newRoot;
 
     public SwapRootEndEventHandler(LineElement lineElement, BinaryHeapNode parentLeaf, BinaryHeapNode first, BinaryHeapNode second) {
-        this.lineElement = lineElement;
+        this.lineElementToLeaf = lineElement;
         this.parentLeaf = parentLeaf;
-        this.first = first;
-        this.second = second;
+        this.oldRoot = first;
+        this.newRoot = second;
     }
 
     @Override
     protected void handleForward(ActionEvent actionEvent) {
-        System.out.println("handle forward");
-        FadesTransitionBuilder.getTransition(first.getChildLine(NodePosition.LEFT),Duration.millis(1),1,0).play();
-        FadesTransitionBuilder.getTransition(first.getChildLine(NodePosition.RIGHT),Duration.millis(1),1,0).play();
-        lineElement.setStart(parentLeaf.getChildConnector(isLeft?1:0));
-        lineElement.setEnd(first);
-        FadesTransitionBuilder.getTransition(lineElement,Duration.millis(1),0,1).play();
-
+        System.out.println("moving end - forward ");
+        buildHandle(oldRoot,newRoot);
     }
 
     @Override
     protected void handleBack(ActionEvent actionEvent) {
-        System.out.println("handle back");
+        System.out.println("moving end - back");
+        buildHandle(newRoot,oldRoot);
     }
+
+    private void buildHandle(BinaryHeapNode nRoot,BinaryHeapNode oRoot){
+        //set line from parent leaf
+        lineElementToLeaf.setEnd(nRoot);
+
+        //set line from new root
+        LineElement left = nRoot.getChildLine(NodePosition.LEFT);
+        LineElement right = nRoot.getChildLine(NodePosition.RIGHT);
+        left.setStart(oRoot.getLeftChildConnector());
+        right.setStart(oRoot.getRightChildConnector());
+        oRoot.setChildLine(left, NodePosition.LEFT);
+        oRoot.setChildLine(right, NodePosition.RIGHT);
+
+//        //show line to leaf
+//        showLine(lineElementToLeaf);
+//        //show line from root
+//        showLine(left);
+//        showLine(right);
+    }
+
+    private void showLine(LineElement line){
+//        FadesTransitionBuilder.getTransition(line,Duration.millis(1),0,1).play();
+    }
+
 }
