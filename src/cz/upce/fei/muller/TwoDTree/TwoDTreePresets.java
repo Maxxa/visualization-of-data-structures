efccdd673a5c09673472360c53815f3ec011dcbe
Vojtěch Müller <muller.voj@gmail.com>
2015-05-21 16:23:12 +0200

    add presset for TwoDTree

--- src/cz/upce/fei/muller/TwoDTree/TwoDTreePresets.java
+++ src/cz/upce/fei/muller/TwoDTree/TwoDTreePresets.java
@@ -42,25 +42,45 @@ public class TwoDTreePresets implements Preset<Coordinate, TwoDTreePresetItem> {
 
     private Coordinate[] getSet1() {
         return new Coordinate[]{
-            new Coordinate(26,31),
-            new Coordinate(52,46),
-            new Coordinate(92,12),
+                new Coordinate(26, 31),
+                new Coordinate(52, 46),
+                new Coordinate(92, 12),
+                new Coordinate(149, 79),
+                new Coordinate(110, 180),
+                new Coordinate(5, 120),
+                new Coordinate(16, 49),
         };
     }
 
     private Coordinate[] getSet2() {
         return new Coordinate[]{
-                new Coordinate(100,100),
-                new Coordinate(50,100),
-                new Coordinate(150,10),
-                new Coordinate(15,10),
-                new Coordinate(190,10),
+                new Coordinate(100, 100),
+                new Coordinate(50, 100),
+                new Coordinate(150, 10),
+                new Coordinate(15, 10),
+                new Coordinate(190, 10),
+                new Coordinate(60, 70),
+                new Coordinate(29, 39),
+                new Coordinate(190, 180),
+                new Coordinate(145, 19),
+                new Coordinate(71, 12),
+                new Coordinate(17, 25),
+
         };
     }
 
     private Coordinate[] getSet3() {
         return new Coordinate[]{
-
+                new Coordinate(75, 50),
+                new Coordinate(15, 90),
+                new Coordinate(120, 30),
+                new Coordinate(40, 190),
+                new Coordinate(172, 15),
+                new Coordinate(90, 136),
+                new Coordinate(45, 20),
+                new Coordinate(175, 95),
+                new Coordinate(12, 14),
+                new Coordinate(147, 19),
         };
     }
 
